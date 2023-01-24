6356a430f90c8073d11d05e662cbda14513eb459
Anderson Pimenta <anderson_ps17@hotmail.com>
2023-01-24 00:57:44 -0300

    Desafio concluídogit status!

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,19 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // IMPLEMENTADO
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            // IMPLEMENTADO
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+// IMPLEMENTADO
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia(numero: "123456789", modelo: "Tijolão", imei: "001100110011", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("DIO");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone(numero: "123456789", modelo: "XS", imei: "110011001100", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("DIO");
+
+Console.WriteLine("\n");
